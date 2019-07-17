6e0245039e2cb54156c8e661edb55e1fa1c485cd
lgm527 <laurellmccaffrey@gmail.com>
2019-07-17 17:11:32 -0400

    cr done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,36 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  #index
+  get "/artists" do
+    @artists = Artist.all
+    erb :index
+  end
+
+  #new
+  get "/artists/new" do
+    erb :new
+  end
+
+  #create
+  post "/artists" do
+    @artist = Artist.create(params[:artist])
+    binding.pry
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  #show
+  get "/artists/:id" do
+    find_artist
+    erb :show
+  end
+
+  #edit
+  #update
+  #destroy
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
 end
